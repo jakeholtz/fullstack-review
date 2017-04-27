51cc53ce17f79df5adc0121863a0175a5bace7bb
Jake Holtz <jahholtz@gmail.com>
2017-04-26 20:00:52 -0700

    Added bodyParser. Express - In Progress

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos/import', function (req, res) {
   // console.log(JSON.stringify(req));
-  //
+  // //
   // res.send('POST request to homepage');
+  // .get('http://google.com/img.png')
+// .on('response', function(response) {
+//   console.log(response.statusCode) // 200
+//   console.log(response.headers['content-type']) // 'image/png'
+// })
 
   // Complete the /repos/import route on your express server -
   // in this route, you'll use the npm request library to fetch that
