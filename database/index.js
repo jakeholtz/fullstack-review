ad9b74f29a86450fe594e5f41793cccde49769e4
Jake Holtz <jahholtz@gmail.com>
2017-04-27 12:24:10 -0700

    Implemented basic express route in /repos/import to connect with githubs API

--- database/index.js
+++ database/index.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+// var db = mongoose.connection;
+//   db.on('error', console.error.bind(console, 'connection error:'));
+//   db.once('open', function() {
+//   console.log("We're connected!");
+// });
+
 var repoSchema = mongoose.Schema({
   id:  Number,
   name: String,
--- server/index.js
+++ server/index.js
@@ -1,21 +1,40 @@
 var express = require('express');
 var app = express();
+var request = require('request');
 var bodyParser = require('body-parser')
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json())
 
 app.post('/repos/import', function (req, res) {
-  var url = req.body.url;
-  return function() {
-    null;
-  }
+
+  let username = req.body.username;
+
+  request({
+    url: 'https://api.github.com',
+    method: 'GET',
+    headers: {'user-agent': 'jakeholtz'},
+    authorization: '1e01d19a296bd3c2cb9425b8f001482148d1ff48',
+    data: username
+    }, function (error, response, body) {
+      if (error) {
+        console.log('REQUEST error:', error);
+        console.log('REQUEST statusCode:', response && response.statusCode);
+        console.log('REQUEST body:', body);
+      } else {
+        console.log('SUCCESS!')
+      }
+    }
+  )
+
+  // VIA EXPRESS when you get a post req from client(with username)
+  // if successful
+    // use npm library to connect with github library
+    // get all repos with username
 
   // Complete the /repos/import route on your express server -
   // in this route, you'll use the npm request library to fetch that
   // user's Github repositories from the Github API.
-
-  // TODO
 });
 
 app.get('/repos', function (req, res) {
